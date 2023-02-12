68f689b9e6bcc39dfbb9cba2b9f9fa2092c4c678
jlazaro <you@example.com>
2023-02-12 23:06:22 +0100

    se conecta api xano, el juego ya funciona

--- src/components/TheEnd.vue
+++ src/components/TheEnd.vue
@@ -1,14 +1,23 @@
 <script setup>
+import { ref } from 'vue';
+import { apiClient } from '../js/apiClient';
+import { API_RANKING } from '../js/const';
 import {pad} from '../js/utils'
 import  { useResultStore } from '../stores/result';
+import  { useGridStore } from '../stores/grid';
+import { storeToRefs } from 'pinia';
 
 const useResult = useResultStore()
+const useGrid = useGridStore()
 const {resultsArr} = useResult
+const {isEndView, isRankingView} = storeToRefs(useGrid)
+const userName = ref('')
+const showInput = ref(true)
 
 let initialValue = {
     time: 0,
     mistakes: 0,
-    points: 0,
+    points: 2000,
 }
 
 let totalRounds = resultsArr.reduce((acc, currentValue) => {
@@ -30,6 +39,24 @@ function setSeconds(secs) {
       return pad(parseInt(secs % 60));
 }
 
+
+async function sendData(){
+    let data = {
+        name: userName.value,
+        points: totalRounds.points
+    }
+    try {
+        let res = await apiClient.post(API_RANKING.concat('user'), data)
+        showInput.value = false
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function goToRanking(){
+    isEndView.value = false
+    isRankingView.value = true
+}
 </script>
 
 <template>
@@ -61,13 +88,22 @@ function setSeconds(secs) {
             </tr>
         </tfoot>
     </table>
-    <p class="save-para">¡Guarda tu puntuación!</p>
-    <form action="" @submit.prevent="" class="form">
-        <input type="text" placeholder="Nombre o nickname">
-        <button class="btn" @click="">Envíar puntuación</button>
-    </form>
+    <div v-if="showInput">
+        <p class="save-para">¡Guarda tu puntuación!</p>
+        <form action="" @submit.prevent="sendData()" class="form">
+            <input type="text" placeholder="Nombre o nickname" v-model="userName" minlength="5" maxlength="15">
+            <button class="btn" @submit="">Envíar puntuación</button>
+        </form>
+    </div>
+    <div class="success" v-else>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+          <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+        </svg>
+        Puntuación enviada
+    </div>
     <div class="ranking">
-        <a href="">Ver Ranking</a> 
+        <a @click="goToRanking()">Ver Ranking</a> 
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
         </svg>
@@ -105,11 +141,15 @@ p{
     display: flex;
     gap: 0.3rem;
     align-items: center;
-    margin: 0 0 1rem 0;
+    margin: 1rem 0;
 }
 
 .save-para{
     margin-bottom: 0.5rem;
+}
+
+.success{
+    color: rgb(0, 175, 0)
 }
 
 tfoot > tr > td {
